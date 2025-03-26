e74775d7bbf47365ff42936893353d763e85321b
gratidutra <gratirodrigues.gdr@gmail.com>
2025-03-26 17:09:30 -0300

    return in list

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import streamlit as st
-from metapub import PubMedFetcher, MetaPubError
+from metapub import PubMedFetcher
 from dotenv import load_dotenv
 import re
 
-# Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Inicializa o fetcher
 fetch = PubMedFetcher()
 
-# Função para validar se o DOI tem o formato correto
-def is_valid_doi(doi):
-    return bool(re.match(r"^10.\d{4,9}/[-._;()/:A-Z0-9]+$", doi, re.IGNORECASE))
 
-# Função para obter os PMIDs
 def get_pmid(dois):
     list_pmid = []
 
     for doi in dois:
-        if not is_valid_doi(doi):
-            print(f"Invalid DOI format: {doi}, skipping.")
-            continue
-
         try:
             pmids = fetch.article_by_doi(doi)
             if pmids and hasattr(pmids, 'pmid'):
                 list_pmid.append(pmids.pmid)
             else:
                 print(f"No PMID found for DOI: {doi}, skipping.")
-        except MetaPubError as e:
+        except Exception as e:
             print(f"Error fetching PMID for DOI {doi}: {str(e)}")
             continue
     
@@ -37,26 +27,21 @@ def get_pmid(dois):
     # Retorna a lista de PMIDs
     return list_pmid
 
-# Interface Streamlit
+
 st.title('PubMed DOI to PMID Fetcher')
 
-# Caixa de texto para inserir DOIs
 dois_input = st.text_area("Insira os DOIs (um por linha)", height=200)
 
-# Quando o botão for pressionado
 if st.button("Obter PMIDs"):
-    # Separar os DOIs em uma lista
     dois_list = [doi.strip() for doi in dois_input.split("\n") if doi.strip()]
     
     if dois_list:
-        # Chamar a função get_pmid
         pmids = get_pmid(dois_list)
         
         if pmids:
-            # Exibir os PMIDs encontrados
-            pmids_string = ', '.join(pmids)  # Lista de PMIDs separada por vírgulas
+            pmids_string = ', '.join(pmids)  
             st.write("PMIDs encontrados:")
-            st.write(pmids_string)  # Exibe os PMIDs como uma string separada por vírgulas
+            st.write(pmids_string)  
         else:
             st.write("Nenhum PMID encontrado.")
     else:
